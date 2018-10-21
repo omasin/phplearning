a67ff154c4289a3ec736e108d037ccc6eda1a944
root <root@ip-10-0-0-201.ap-northeast-1.compute.internal>
2018-10-21 11:31:04 +0000

    DB access OK

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 define('DB_DATABASE', 'dotinstall_db');
 define('DB_USERNAME', 'dbuser');
 define('DB_PASSWORD', 'dbuser');
-define('PDO_DSN', 'mysql:host=localhost;dbname=' . DB_DATABASE);
+define('PDO_DSN', 'mysql:host=shintaroh-rds-mysql-001.c43pcgs4eqtc.ap-northeast-1.rds.amazonaws.com;dbname=' . DB_DATABASE);
 
 try {
 	// connect
-	$db = new PDO(PDN_DNS, DB_USERNAME, DB_PASSWORD);
+	$db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	// insert
